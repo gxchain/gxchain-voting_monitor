123a2dd1b82b1f7b456dadab9049ff5916d20672
sspin <826015751@qq.com>
2021-11-15 18:40:56 +0800

    chore: update voter api

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -8,10 +8,36 @@ const router = express.Router();
 
 router.get("/voter", async (req, res) => {
   try {
-    let result = await Voter.findAll();
-    let resultTrue = await Voter.findAll({ where: { votingstate: true } });
-    let resultFalse = await Voter.findAll({ where: { votingstate: false } });
-    res.send({ result, resultTrue, resultFalse });
+    const offset = req.query.offset ? Number(req.query.offset) : 0;
+    const limit = req.query.limit ? Number(req.query.limit) : 10;
+    const resultsSum = await Voter.count();
+    let result = await Voter.findAll({ offset: offset, limit: limit });
+    res.send({ result, resultsSum });
+  } catch (err) {
+    throw err;
+  }
+});
+
+router.get("/voter_sum", async (req, res) => {
+  try {
+    const voterNum = await Voter.count();
+    const voterTrueNum = await Voter.count({ where: { votingstate: true } });
+    const voterFalseNum = await Voter.count({ where: { votingstate: false } });
+    const voterGXCSum = await Voter.sum("voteGXCNumberHourly");
+    const voterGXCTrueSum = await Voter.sum("voteGXCNumberHourly", {
+      where: { votingstate: true },
+    });
+    const voterGXCFalseSum = await Voter.sum("voteGXCNumberHourly", {
+      where: { votingstate: false },
+    });
+    res.send({
+      voterNum,
+      voterTrueNum,
+      voterFalseNum,
+      voterGXCSum,
+      voterGXCTrueSum,
+      voterGXCFalseSum,
+    });
   } catch (err) {
     throw err;
   }
